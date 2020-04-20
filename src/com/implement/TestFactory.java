5f8478cd41a81a3e447d8a018f167e9ddb41b09d
AbhishekBalaramRaut <abhirait874@gmail.com>
2020-04-20 12:49:36 -0700

    HQL Total Employees

--- src/com/implement/TestFactory.java
+++ src/com/implement/TestFactory.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 //import org.hibernate.cfg.AnnotationConfiguration;
@@ -22,46 +23,19 @@ import com.model.Vehicle;
 public class TestFactory {
 
 	public static void main(String[] args) {
-		Book book  = new Book();
-		
-		book.setBookid("16");
-		book.setBookname("Harry Potter 2");
-		book.setAuthorid("rowling 1");
-		Address a1 = new Address();
-		a1.setState("Maharashtra");
-		a1.setCity("Mumbai");
-		a1.setStreet("rane street");
-		
-		Address a2 = new Address();
-		a2.setState("Maharashtra");
-		a2.setCity("Pune");
-		a2.setStreet(" shaniwar peth");
 
-		
-		Employee emp1  = new Employee();
-		emp1.setEmployeeName("Abhishek");
-		emp1.setJoiningDate(new Date());
-		
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-	    emp1 = (Employee)session.get(Employee.class, 3);
+		
+		Query<Employee> query = session.createQuery("from Employee");
+		List<Employee> empList = query.list();
 	    
 		session.getTransaction().commit();
 		session.close();
 		
-		
-
-		
-		session = sessionFactory.openSession();
-		session.beginTransaction();
-	    session.delete(emp1);
-	  
-		session.getTransaction().commit();
-		session.close();
-		
-		System.out.println(emp1);
+		System.out.println("Total employees "+empList.size());
 		sessionFactory.close();
 		
 	}
